733739e8cbb750ae0cd2bc7872939469fd3a8700
AEve70 <evemh19@gmail.com>
2025-11-08 01:28:27 -0600

    Base Cliente; Los datos se van a solicitar en el forms

---
+++ AppClienteControlador/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppClienteControlador
+{
+    public class Cliente
+    {
+        private TcpClient cliente;
+        private StreamReader reader;
+        private StreamWriter writer;
+
+
+        public bool Conectado => cliente?.Connected ?? false; //Expresion remplaza if
+
+        public async Task<bool> Conectar(String ip, int puerto)
+        {
+            try
+            {
+                cliente = new TcpClient();
+                await cliente.ConnectAsync(ip, puerto);
+
+                NetworkStream stream = cliente.GetStream();
+                writer = new StreamWriter(stream) {AutoFlush = true };
+                reader = new StreamReader(stream);
+
+                Console.WriteLine("Conectado al servidor");
+                return true;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                return false;
+            }
+        } 
+    }
+}
