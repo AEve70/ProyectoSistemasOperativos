9ef65a82b3343d25db5ffe08a9c51bb51d3d8813
AEve70 <evemh19@gmail.com>
2025-11-08 01:07:02 -0600

    Handler logica basica agregada

--- AppServidor/Handler.cs
+++ AppServidor/Handler.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace AppServidor
 {
-    class Handler
+    public class Handler
     {
+        private TcpClient cliente;
+
+        public Handler(TcpClient cliente)
+        {
+            this.cliente = cliente;
+        }
+
+
+        public void Run()
+        {
+            try
+            {
+                using NetworkStream stream = cliente.GetStream();
+                using StreamReader reader = new StreamReader(stream);
+                using StreamWriter writer = new StreamWriter(stream);
+
+                String linea;
+
+                while((linea = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine("Cliente {0} : {1}", cliente.Client.RemoteEndPoint, linea);
+
+                    writer.WriteLine("Recibido");
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine("Error " + e);
+            }
+            finally
+            {
+                cliente.Close();
+                Console.WriteLine("La conexiòn ha finalizado");
+            }
+        }
     }
 }
