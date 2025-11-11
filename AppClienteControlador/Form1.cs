eed42618adc1f496528d37dfc99d4066407c40f6
AEve70 <evemh19@gmail.com>
2025-11-11 00:31:51 -0600

    Conexion al servidor lisya

--- AppClienteControlador/Form1.cs
+++ AppClienteControlador/Form1.cs
@@ -12,9 +12,12 @@ namespace AppClienteControlador
 {
     public partial class Form1 : Form
     {
+        Cliente client;
         public Form1()
         {
             InitializeComponent();
+            client = new Cliente();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,9 +35,29 @@ namespace AppClienteControlador
 
         }
 
-        private void btn_conectar_Click(object sender, EventArgs e)
+        private async void btn_conectar_Click(object sender, EventArgs e)
         {
+            string ip = txt_ip.Text.Trim();
+            int puerto = int.Parse(txt_puerto.Text);
 
+            label4.Text = "Conectando...";
+            label4.ForeColor = Color.Goldenrod;
+            btn_conectar.Enabled = false;
+
+            bool ok = await client.Conectar(ip, puerto);
+
+            if (ok)
+            {
+                label4.Text = "Conectado";
+                label4.ForeColor = Color.ForestGreen;
+            }
+            else
+            {
+                label4.Text = "Error de conexión";
+                label4.ForeColor = Color.Red;
+            }
+
+            btn_conectar.Enabled = true;
         }
     }
 }
--- AppServidor/Program.cs
+++ AppServidor/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AppServidor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Console.Title = "Servidor TCP";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("=== Servidor remoto iniciado ===");
+
+            int puerto = 8000;
+            Servidor servidor = new Servidor(puerto);
+
+            try
+            {
+                await servidor.IniciarServidor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error al iniciar el servidor: " + ex.Message);
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
