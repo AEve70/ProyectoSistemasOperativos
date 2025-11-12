14156c6f1bee40e982c0eed06fc0beeaba7c706c
AEve70 <evemh19@gmail.com>
2025-11-11 22:54:03 -0600

    Compartir conexion

---
+++ MensajesIO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Compartido
+{
+    public class MensajesIO
+    {
+        public string Comando { get; set; }
+        public bool Ok { get; set; }
+        public object Datos { get; set; }
+        public string Mensaje { get; set; }
+        public string Enviador { get; set; }
+        public string Tiempo { get; set; }
+
+        public MensajesIO() => Tiempo = DateTime.Now.ToString("o");
+
+        public MensajesIO(string comando, bool ok = true, object datos = null, string mensaje = "", string enviador = "")
+        {
+            Comando = comando;
+            Ok = ok;
+            Datos = datos;
+            Mensaje = mensaje;
+            Enviador = enviador;
+            Tiempo = DateTime.Now.ToString("o");
+        }
+    }
+}
